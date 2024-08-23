7fdf3812460cd59e21ff4f72316e6027031ade85
Форов Александр <5h9t5@list.ru>
2024-08-23 17:25:45 +0300

    feat(filters/car): add alias

--- app/filters/car.py
+++ app/filters/car.py
@@ -1,5 +1,7 @@
 from fastapi_filter.contrib.sqlalchemy.filter import Filter
 
+from pydantic import Field
+
 from app.models.car import Car
 
 
@@ -9,12 +11,15 @@ class CarFilter(Filter):
     year: int | None = None
     fuel_type: str | None = None
     transmission: str | None = None
-    mileage__gte: int | None = None
-    mileage__lte: int | None = None
-    price__gte: int | None = None
-    price__lte: int | None = None
+    mileage__gte: int | None = Field(None, alias='mileage_min')
+    mileage__lte: int | None = Field(None, alias='mileage_max')
+    price__gte: int | None = Field(None, alias='price_min')
+    price__lte: int | None = Field(None, alias='price_max')
 
     order_by: list[str] = []
 
     class Constants(Filter.Constants):
         model = Car
+
+    class Config:
+        extra = "allow"
